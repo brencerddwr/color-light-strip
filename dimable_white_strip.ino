41a1aa86ea0f90e5859f6d8735329247279ed6a4
brencerddwr <rgbailey@gmail.com>
2014-12-21 20:26:45 -0800

    add part of code for second strip, enable code for hue and saturation adjustment, start adding code for mode switch.

--- dimable_white_strip.ino
+++ dimable_white_strip.ino
@@ -1,23 +1,38 @@
+/*  This is for a dimable lighting strip for our travel trailer running off the 12vdc coach system.
+this version is configured for two Radio Shack led strips of different lengths, one is 10 clusters and
+the other is 8 clusters. They are set up in a single LED array.
+
+This uses the FastLED library that can control many different types of addressable leds and it should be
+possible to even mix makes and types of arrays if you use a different data pin for each strip.
+
+Your mileage may vary and it is not my fault if your computer ceases to exist, you gain a cat, etc.
+*/
+
 #include <FastLED.h>
 
-// How many leds are in the strip?
-#define NUM_LEDS 10
+// how many led strips  This is not actually used anywhere, it is a reference to help setup the strips and array
+#define NUM_LED_STRIPS 2
+// How many leds are in EACH strip?
+#define NUM_LEDS1 10
+#define NUM_LEDS2 8
 
 // Data pin that led data will be written out over
-#define DATA_PIN 11
-
+#define DATA_PIN1 10
+#define DATA_PIN2 11
+#define Total_LEDS  NUM_LEDS1+NUM_LEDS2
 // This is an array of leds.  One item for each led in your strip.
-CRGB leds[NUM_LEDS];
+CRGB leds[Total_LEDS];
 
 // setup inputs
 const int brightness_pot = A0;  // Analog input pin that the potentiometer is attached to for brightness
 int sensorValue_brightness = 0;        // value read from the brightness pot
-
-/*const int hue_pot = A1;  // Analog input pin that the potentiometer is attached to for hue
+const int hue_pot = A1;  // Analog input pin that the potentiometer is attached to for hue
 int sensorValue_hue = 0;        // value read from the hue pot
 const int saturation_pot = A2;  // Analog input pin that the potentiometer is attached to for brightness
 int sensorValue_saturation = 0;        // value read from the saturation pot
-*/
+#define WHITE_MODE 2  // digital input pin for white mode
+#define BLUE_MODE 4  // digital input pin for blue mode
+
 
 // ***********************************************************************************************************
 // *
@@ -31,11 +46,12 @@ void setup() {
 	delay(2000);
 
 	// Change this line as needed to match the LED string type, control chip and color sequence
-	FastLED.addLeds<TM1803, DATA_PIN, GBR>(leds, NUM_LEDS); // RadioShack LED String
+	FastLED.addLeds<TM1803, DATA_PIN1, GBR>(leds, 0, NUM_LEDS1); // RadioShack LED String
+	FastLED.addLeds<TM1803, DATA_PIN2, GBR>(leds, NUM_LEDS1,NUM_LEDS2); //RadioShack LED String
 	FastLED.setDither(false); //disable FastLED dithering, was causing a flicker at some brightness levels
 
 	// turn off all leds
-	fill_solid ( &(leds[0]), NUM_LEDS, CRGB::Black);
+	fill_solid ( &(leds[0]),Total_LEDS, CRGB::Black);
 	FastLED.show();
 	delay(1000);
 }
@@ -53,10 +69,12 @@ void loop()
 	static uint8_t saturation = 0;
 	sensorValue_brightness = analogRead(brightness_pot);
 	brightness = map(sensorValue_brightness, 0, 1023, 2, 255);
-/*	sensorValue_hue = analogRead(hue_pot);
+	sensorValue_hue = analogRead(hue_pot);
 	hue = map(sensorValue_hue, 0,1023,0,255);
 	sensorValue_saturation = analogRead(saturation_pot);
-	saturation = map(sensorValue_saturation,0,1023,0,255); */
+	saturation = map(sensorValue_saturation,0,1023,0,255);
+	/*to do
+	insert if statements for white and blue modes*/
 	FastLED.setBrightness(brightness);
 	FastLED.showColor(CHSV(hue, saturation, 255));
 
